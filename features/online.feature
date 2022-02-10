a6573446e22609db0d29a1f35a4f05c5905b95af
rathavaharish <rathavaharish@gmail.com>
2022-02-10 21:15:50 +0530

    Add new feature

--- features/online.feature
+++ features/online.feature
@@ -6,3 +6,11 @@ Feature: Login and add product in card
     Given I am Login Page
     Then Add product in Cart
     Then verify cart price
+    Then Send message from contact page
+    Then Make Payment for cart added order
+    Then Search T-Shirt in Serchbox
+    Then Open order history detail
+    Then Open My Credit Slips
+    Then Open My Addresses
+    Then Open My Personal Information
+    Then Open My Wishlists
--- features/step-definitions/common.step.ts
+++ features/step-definitions/common.step.ts
@@ -38,10 +38,7 @@ await   ajax_add_to_cart_button.click()
 
 const   checkout = await $('=Proceed to checkout');
 await   checkout.click() 
- 
-const   standard = await $('.standard-checkout');
-await   standard.click() 
-  
+   
 })
 Then(/^verify cart price$/,async () => {    
     const   shopping_cart = await $('.shopping_cart a');
@@ -52,5 +49,89 @@ Then(/^verify cart price$/,async () => {
     const pricematch = $('#total_product_price_1_1_635671')
     await expect(pricematch).toHaveTextContaining('$16.51')
 
+})
+Then(/^Send message from contact page$/,async () => {    
+    const   contact = await $('#contact-link');
+    await   contact.click();    
+
+    const   msgTextArea = await $('#message');
+    await   msgTextArea.addValue("Hello I am Harish")
+   
+
+    const selectBox = await $('#id_contact');
+
+    await selectBox.selectByAttribute('value', '2');
+
+    const submitMessage = await $('#submitMessage');
+    await submitMessage.click();
+
+    browser.pause(5000);
+
+})
+Then(/^Make Payment for cart added order$/,async () => {
+
+    const   shopping_cart = await $('.shopping_cart a');
+    await   shopping_cart.click() 
+
+    const   standard = await $('.standard-checkout');
+    await   standard.click() 
+
+    const   cartcheckout = await $('.cart_navigation button');
+    await   cartcheckout.click() 
+    
+    const   cgv = await $('#cgv');
+    await   cgv.click() 
+
+    const   standardcheckout = await $('.standard-checkout');
+    await   standardcheckout.click() 
+
+    const   bankwire = await $('.bankwire');
+    await   bankwire.click()   
+
+    
+})
+Then(/^Search T-Shirt in Serchbox$/,async () => {   
+    const search_query_top = await $('#search_query_top');
+    await search_query_top.setValue('T-Shirt');
+    
+    const buttonSearch = await $('#search_query_top');
+    await buttonSearch.click();
+    
+    browser.pause(5000);
+})
+Then(/^Open order history detail$/,async () => {
+    const account = await $(".account");
+    await account.click(); 
+    browser.pause(5000);
+    const orderHistoryAndDetail = await $(".icon-list-ol");
+    await orderHistoryAndDetail.click();     
+});
+Then(/^Open My Credit Slips$/,async () => {
+    const myaccount = await $("=My account");
+    await myaccount.click(); 
+
+    const myCreditSlip = await $(".icon-ban-circle");
+    await myCreditSlip.click();    
+})
+Then(/^Open My Addresses$/,async () => {
+    const myaccount = await $("=My account");
+    await myaccount.click(); 
+
+    const myAddresses = await $(".icon-building");
+    await myAddresses.click();    
+})
+Then(/^Open My Personal Information$/,async () => {
+    const myaccount = await $("=My account");
+    await myaccount.click(); 
+
+    const mypersonalInfo = await $(".icon-user");
+    await mypersonalInfo.click();    
+})
+Then(/^Open My Wishlists$/,async () => {
+    const myaccount = await $("=My account");
+    await myaccount.click(); 
+
+    const myCreditSlip = await $(".icon-heart");
+    await myCreditSlip.click();    
 })
 
